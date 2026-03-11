3e4cafdb2655387d705154b20f69a4fd6359d53c
world <world@worlds-MacBook-Air.local>
2026-03-11 21:14:49 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,75 @@
+# ~/.config/nix/flake.nix
+
+{
+  description = "My system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+        url = "github:LnL7/nix-darwin";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+        url = "github:nix-community/home-manager"
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = {pkgs, ... }: {
+        nix.enable = false;
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility. please read the changelog
+        # before changing: `darwin-rebuild changelog`.
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        # If you're on an Intel system, replace with "x86_64-darwin"
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # Declare the user that will be running `nix-darwin`.
+        users.users.world = {
+            name = "world";
+            home = "/Users/world";
+        };
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;
+
+        environment.systemPackages = [ pkgs.fastfetch pkgs.vim pkgs.ripgrep pkgs.tmux ];
+        security.pam.enableSudoTouchIdAuth = true;  
+        homeconfig = {pkgs, ...}: {
+            # this is internal compatibility configuration 
+            # for home-manager, don't change this!
+            home.stateVersion = "23.05";
+            # Let home-manager install and manage itself.
+            programs.home-manager.enable = true;
+
+            home.packages = with pkgs; [];
+
+            home.sessionVariables = {
+                EDITOR = "vim";
+            };
+        };
+  };
+  in
+  {
+    darwinConfigurations.worlds-MacBook-Air = nix-darwin.lib.darwinSystem {
+      modules = [
+         configuration
+      
+         home-manager.darwinModules.home-manager  {
+             home-manager.useGlobalPkgs = true;
+             home-manager.useUserPackages = true;
+             home-manager.verbose = true;
+             home-manager.users.$USER = homeconfig;
+          }
+       ];
+    };
+  };
+}
